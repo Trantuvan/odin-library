256a680be7abbe96b50221452a635f0cfc8f9d57
trantuvan <trantuvan.kan@gmail.com>
2022-07-12 21:59:36 +0700

    Add JS for modal to function

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// Modal
+const addBookbtn = document.querySelector("#add-book-btn");
+const modal = document.querySelector(".modal");
+const modalContainer = document.querySelector(".modal-container");
+const closeModalbtn = document.querySelector(".close__logo");
+
+addBookbtn.addEventListener("click", openModal);
+
+// close modal when clicked x button
+closeModalbtn.addEventListener("click", closeModal);
+
+// close modal when clicked outside of modal container
+modal.addEventListener("click", closeModal);
+
+// stop modal disappear when clicked inside modal container
+modalContainer.addEventListener("click", (evt) => evt.stopPropagation());
+
+function openModal() {
+  if (modal.classList.contains("deactivated")) {
+    modal.classList.remove("deactivated");
+  }
+}
+
+function closeModal() {
+  if (modal.classList.contains("deactivated") === false) {
+    modal.classList.add("deactivated");
+  }
+}
